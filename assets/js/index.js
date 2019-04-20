26b71763af7f0d6ee46322006280e4f5c7d607da
Jireh <jjnimes@gmail.com>
2019-04-20 22:59:12 +0800

    Added navigation bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,3 @@
-VANTA.WAVES({
-    el: "#nav",
-    color: 0x294150,
-    shininess: 19.00,
-    waveHeight: 17.50,
-    waveSpeed: 0.40,
-    zoom: 1.29
-})
+$(function() {
+    $('nav').addClass('index');
+});
